ba0b6920c5b178ec4903293e0ac42debfdaa197a
深谷もも <fukaya@orc-net.com>
2019-11-21 15:50:20 +0900

    プロジェクト作成？

---
+++ dotPict/dotPict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dotPict
+//
+//  Created by test on 2019/11/21.
+//  Copyright © 2019年 momo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
